5bad741bf6dec13c44c44c04d4089722fec7a0f1
tnelsonmiller <41117266+tnelsonmiller@users.noreply.github.com>
2018-12-04 12:32:47 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -11,4 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
 
-serviceWorker.register();
+// serviceWorker.register();
